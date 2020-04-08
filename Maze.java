91c4864d99c6e0ea2fb0c54ecdc5992f3286c6d4
Vicky <20258258@student.curtin.edu.au>
2020-04-08 22:27:58 +0400

    Update if_replace changes updated

--- Maze.java
+++ Maze.java
@@ -79,25 +79,26 @@ public class Maze
             }
             else
             {
-                if(grid[currentRow][currentCol] == EMPTY)
-                {
-                    grid[currentRow][currentCol] = VISITED;
-                }
-                else if(grid[currentRow][currentCol] == WALL)
-                {
-                    done = true;
-                    System.out.println("You stumble blindly into a solid concrete wall."); // Hit wall.
-                }
-                else if(grid[currentRow][currentCol] == END)
-                {
-                    done = true;
-                    solved = true;
-                    System.out.println("SOLVED!"); // Solved.
-                }
-                else
-                {} // Do nothing
-            }
-            
+switch(grid[currentRow][currentCol])
+{
+    case Maze.EMPTY:
+         grid[currentRow][currentCol] = Maze.Visited;
+         break;
+    
+    case Maze.WAll
+         done = true;
+         System.out.println("You stumble blindly into a solid concrete wall.");
+         break;    
+          
+    case Maze.END
+          done = true;
+          solved = true;
+          System.out.println("SOLVED!"); 
+          break;
+              
+     default; 
+         // Do nothing
+}          
             charIndex++;
         }
         
